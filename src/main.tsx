c7c4007bcd2289dcb971d347ac3c00c5d8319351
ivanm696 <59553806+ivanm696@users.noreply.github.com>
2026-01-02 21:08:17 +0200

    Create main.tsx
    import React from 'react'
    import ReactDOM from 'react-dom/client'
    import BoltClone from './components/BoltClone'
    import './index.css'
    
    ReactDOM.createRoot(document.getElementById('root')!).render(
      <React.StrictMode>
        <BoltClone />
      </React.StrictMode>
    )

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BoltClone from './components/BoltClone'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BoltClone />
+  </React.StrictMode>
+)
